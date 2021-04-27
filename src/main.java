731a7a65a438c8aed04dac7d3214c4838d68f782
umurislamoglu <74820307+umurislamoglu@users.noreply.github.com>
2021-04-27 13:57:40 +0300

    First Commit

---
+++ src/main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main {
+	
+	static void fizzBuzz() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Number : ");
+		int number = scanner.nextInt();
+		
+		
+		if(number%3 == 0 && number%5 == 0) 
+			System.out.println("FizzBuzz");
+		 else if (number%5 == 0)
+			System.out.println("Fizz");
+		  else if (number%3 == 0)
+				System.out.println("Buzz");
+		  else
+			  System.out.println(number);
+		fizzBuzz();
+	};
+
+	public static void main(String[] args) {
+		
+		fizzBuzz();
+		
+		
+	}
+
+}
